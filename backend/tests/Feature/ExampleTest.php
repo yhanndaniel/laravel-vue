c5bdf9e32de2152de5679aafd3bbc9d04294d211
Yhann Daniel <yhanndaniel@gmail.com>
2023-07-25 06:56:00 -0300

    Install PEST

--- backend/tests/Feature/ExampleTest.php
+++ backend/tests/Feature/ExampleTest.php
@@ -1,19 +1,10 @@
 <?php
 
-namespace Tests\Feature;
+/**
+ * A basic test example.
+ */
+test('the application returns a successful response', function () {
+    $response = $this->get('/');
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
-
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
-}
+    $response->assertStatus(200);
+});
--- backend/tests/Unit/ExampleTest.php
+++ backend/tests/Unit/ExampleTest.php
@@ -1,16 +1,8 @@
 <?php
 
-namespace Tests\Unit;
-
-use PHPUnit\Framework\TestCase;
-
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     */
-    public function test_that_true_is_true(): void
-    {
-        $this->assertTrue(true);
-    }
-}
+/**
+ * A basic test example.
+ */
+test('that true is true', function () {
+    expect(true)->toBeTrue();
+});
